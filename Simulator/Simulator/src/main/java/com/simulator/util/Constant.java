383b119a7609ad71b615815721726e266fae30e7
daniel <dquinonezlib@gmail.com>
2018-03-26 19:52:07 -0500

    commit test

---
+++ Simulator/Simulator/src/main/java/com/simulator/util/Constant.java
@@ -0,0 +1,8 @@
+package com.simulator.util;
+
+public class Constant {
+   public static final int MIN_AMOUNT = 1000;
+   public static final int MAX_AMOUNT = 15000;
+   public static final int NUMBER_MONTHS = 36;  
+   
+}
